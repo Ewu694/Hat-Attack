096fe292360fdf3dec57f673b6030c47e106d240
justSass <yorgeliscastillo92@gmail.com>
2023-10-19 19:39:58 -0700

    Score

--- GameManager.cs
+++ GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -14,21 +15,26 @@ public class GameManager : MonoBehaviour
     private HashSet<Mole> currentHats = new HashSet<Mole>();
     private int score;
     private bool playing = false;
+    public TextMeshProUGUI ScoreUI;
+    public TextMeshProUGUI Timer;
+
 
     public void GameOver(int type)
     {
-        foreach(Mole hat in hats)
+        foreach (Mole hat in hats)
         {
             hat.StopGame();
         }
         playing = false;
         playButton.SetActive(true);
+        ScoreUI.enabled = false;
+        Timer.enabled = false;
     }
 
     public void StartGame()
     {
         playButton.SetActive(false);//once game ends, hides all the hats
-        for(int i = 0; i < hats.Count; ++i)
+        for (int i = 0; i < hats.Count; ++i)
         {
             hats[i].Hide();
             hats[i].SetIndex(i);
@@ -38,40 +44,51 @@ public class GameManager : MonoBehaviour
         timeRemaining = startingTime;//resets the timer
         score = 0;
         playing = true;
+        ScoreUI.enabled = true;
+        Timer.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playing)
+        if (playing)
         {
             timeRemaining -= Time.deltaTime;
-            if(timeRemaining <= 0)
+
+            if (timeRemaining <= 0)
             {
                 timeRemaining = 0;
                 GameOver(0);
             }
-            if(currentHats.Count <= (score / 10))//every 10 hats, more hats appear
+            if (currentHats.Count <= (score / 10))//every 10 hats, more hats appear
             {
                 int index = Random.Range(0, hats.Count);
                 if (!currentHats.Contains(hats[index]))
                 {
                     currentHats.Add(hats[index]);
                     hats[index].Activate(score / 10);
-                }    
+                }
             }
         }
-   
+        TimerDisplay();
+
+    }
+    public void TimerDisplay()
+    {
+        Timer.text = "Time:" + timeRemaining.ToString();
     }
 
-    public void AddScore(int hatIndex)
+    public void AddScore(int hatIndex, int addscore)
     {
         //to be implemented by Joshua
+        score += addscore;
+        ScoreUI.text = "Score:" + score.ToString();
+        currentHats.Remove(hats[hatIndex]);
     }
 
     public void Missed(int hatIndex, bool isHat)
     {
-        if(isHat)
+        if (isHat)
         {
             timeRemaining -= 3;//decreases the time if a mole goes by unhit
         }
